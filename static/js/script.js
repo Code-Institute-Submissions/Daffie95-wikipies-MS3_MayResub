43f7670f86f1a503bd78ef8e83a4eb6bdec7e0d4
Patrik Svahnström <36499459+Daffie95@users.noreply.github.com>
2021-03-15 08:27:10 +0100

    Allergens update and commenting
    adding allergens description and commenting for JQuerys

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+    // function to activate mobile sidenave
     $(".sidenav").sidenav({ edge: "right" });
+    // function to active collapsible to be clickable
     $('.collapsible').collapsible();
+    // function to activate tooltip on hover
+    $('.tooltipped').tooltip();
 });
