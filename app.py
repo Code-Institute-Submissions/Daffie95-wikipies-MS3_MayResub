010be25d7b0de4791ee69fc66d9cdd6929be63e3
Patrik Svahnström <36499459+Daffie95@users.noreply.github.com>
2021-03-15 12:45:50 +0100

    Update app.py
    comment on login function

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def register():
 # future return statement for profile page
 # return redirect(url_for("profile", username=session["user"]))
 
-
+# function to check wether user credentials are in the database and put user in session
 @app.route("login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
