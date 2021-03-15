d2ceea32f8d9f985232e30d689e0c1e2dbb72ede
Patrik Svahnström <36499459+Daffie95@users.noreply.github.com>
2021-03-15 17:28:41 +0100

    Update app.py
    comments

--- app.py
+++ app.py
@@ -15,23 +15,26 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
+# assigning PyMongo using app(flask) to the variable mongo
 mongo = PyMongo(app)
 
-
+# route function to ensure that any empty routes renders the frontpage
 @app.route("/")
+# route function to render the frontpage and all current recepies
 @app.route("/frontpage")
 def frontpage():
     recipes = list(mongo.db.recipes.find())
     return render_template('front_page.html', recipes=recipes)
 
-
+# route function to render the search function
 @app.route("/search", methods=["GET", "POST"])
 def search():
+    # using indexing on the database to create a search function
     search = request.form.get("search")
     recipes = list(mongo.db.recipes.find({"$text": {"$search": search}}))
     return render_template("front_page.html", recipes=recipes)
 
-
+# route function to render the register page
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -59,7 +62,7 @@ def register():
 
 # function to check wether user credentials are in the database and put user in session
 
-
+# route function to render the login page
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
@@ -88,15 +91,14 @@ def login():
 
     return render_template('login_page.html')
 
-
+# route function to log out current session user with .pop()
 @app.route("/logout")
 def logout():
-    #removes/logs out user from session
     flash("You have logged out")
     session.pop("user")
     return redirect(url_for("login"))
 
-
+# route function to render current session user's profile page
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
     recipes = list(mongo.db.recipes.find())
@@ -105,16 +107,19 @@ def profile(username):
     if session["user"]:
         return render_template("profile_page.html", username=username, recipes=recipes)
 
+# route function for uploading new recipes
 @app.route("/upload_recipe", methods=["GET", "POST"])
 def upload_recipe():
     if request.method == "POST":
+        # function to make the switch toggle on/off in the dictionary "recipe"
         gluten = "on" if request.form.get("gluten") else "off"
         lactose = "on" if request.form.get("lactose") else "off"
         nuts = "on" if request.form.get("nuts") else "off"
         peanuts = "on" if request.form.get("peanuts") else "off"
         shellfish = "on" if request.form.get("shellfish") else "off"
         fish = "on" if request.form.get("fish") else "off"
-
+        # the dictionary pulls content from the input fields on upload_page.html
+        # and inserts it into the database
         recipe = {
             "recipe_name": request.form.get("recipe_name").lower(),
             "category": request.form.get("category").lower(),
@@ -130,6 +135,7 @@ def upload_recipe():
             "recipe_steps": request.form.get("recipe_steps").lower(),
             "created_by": session["user"].lower()
         }
+        # inserts the dictionary "recipe" in the database
         mongo.db.recipes.insert_one(recipe)
         flash("Recipe has been added!")
         return redirect(url_for("frontpage"))
