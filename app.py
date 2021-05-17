856a500af769e120287bfaab40a8345d77a5aa22
Daffie95 <patrik.svahnstrom@gmail.com>
2021-05-17 10:41:13 +0000

    Removed Comments

--- app.py
+++ app.py
@@ -15,44 +15,32 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
-# assigning PyMongo using app(flask) to the variable mongo
 mongo = PyMongo(app)
 
 
-# route function to ensure that any empty routes renders the frontpage
-
-
 @app.route("/")
-# route function to render the frontpage and all current recepies
 @app.route("/frontpage")
 def frontpage():
     recipes = list(mongo.db.recipes.find())
     return render_template('front_page.html', recipes=recipes)
 
-# route function to render the search function
-
 
 @app.route("/search", methods=["GET", "POST"])
 def search():
-    # using indexing on the database to create a search function
     search = request.form.get("search")
     recipes = list(mongo.db.recipes.find({"$text": {"$search": search}}))
     return render_template("front_page.html", recipes=recipes)
-
-# route function to render the register page
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        # looking in database for same username
         user_exists = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
         if user_exists:
             flash("Username already exists")
             return redirect(url_for("register"))
-        # retrieve information from registration form
         register = {
             "firstname": request.form.get("firstname").lower(),
             "lastname": request.form.get("lastname").lower(),
@@ -61,24 +49,19 @@ def register():
                 request.form.get("password").lower())
         }
         mongo.db.users.insert_one(register)
-        # function to check wether user credentials are in
-        #  the database and put user in session
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
         return redirect(url_for("profile", username=session["user"]))
     return render_template('register_page.html')
 
 
-# route function to render the login page
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        # check stored usernames for match with user input
         user_exists = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
         if user_exists:
-            # check hashed password to match it with user input
             if check_password_hash(
                     user_exists["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
@@ -87,18 +70,14 @@ def login():
                 return redirect(url_for(
                     "profile", username=session["user"]))
             else:
-                # flash to show user credentials was incorrect
                 flash("Password and/or Username is Incorrect")
                 return redirect(url_for("login"))
 
         else:
-            # flash to show user credentials was incorrect
             flash("Password and/or Username is Incorrect")
             return redirect(url_for("login"))
 
     return render_template('login_page.html')
-
-# route function to log out current session user with .pop()
 
 
 @app.route("/logout")
@@ -108,7 +87,6 @@ def logout():
     return redirect(url_for("login"))
 
 
-# route function to render current session user's profile page
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
     recipes = list(mongo.db.recipes.find())
@@ -118,22 +96,15 @@ def profile(username):
         return render_template(
             "profile_page.html", username=username, recipes=recipes)
 
-
-# route function for uploading new recipes
 @app.route("/upload_recipe", methods=["GET", "POST"])
 def upload_recipe():
     if request.method == "POST":
-        # function to make the switch toggle on/off
-        # values in the dictionary "recipe"
         gluten = "on" if request.form.get("gluten") else "off"
         lactose = "on" if request.form.get("lactose") else "off"
         nuts = "on" if request.form.get("nuts") else "off"
         peanuts = "on" if request.form.get("peanuts") else "off"
         shellfish = "on" if request.form.get("shellfish") else "off"
         fish = "on" if request.form.get("fish") else "off"
-        # the dictionary pulls content from the
-        #  input fields on upload_page.html
-        # and inserts it into the database
         new_recipe = {
             "recipe_name": request.form.get("recipe_name"),
             "category": request.form.get("category"),
@@ -148,7 +119,6 @@ def upload_recipe():
             "recipe_desc": request.form.get("recipe_desc"),
             "created_by": session["user"]
         }
-        # inserts the dictionary "recipe" in the database
         mongo.db.recipes.insert_one(new_recipe)
         flash("Recipe has been added!")
         return redirect(url_for("frontpage"))
@@ -159,8 +129,6 @@ def upload_recipe():
 @app.route("/edit_recipe/<recipe_id>", methods=["GET", "POST"])
 def edit_recipe(recipe_id):
     if request.method == "POST":
-        # function to make the switch toggle on/off
-        # values in the dictionary "recipe"
         gluten = "on" if request.form.get("gluten") else "off"
         lactose = "on" if request.form.get("lactose") else "off"
         nuts = "on" if request.form.get("nuts") else "off"
--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,4 @@
 $(document).ready(function () {
-    // function to activate mobile sidenav
     $(".sidenav").sidenav({ edge: "right" });
-    // function to active collapsible to be clickable
     $('.collapsible').collapsible();
 });
